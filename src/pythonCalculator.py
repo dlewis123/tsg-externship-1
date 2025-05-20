ee817f84a58c865ef4709766c84aec31b6670407
dlewis123 <dslewis1317@gmail.com>
2025-05-20 14:54:28 -0400

    changed match case to if else statement

--- src/pythonCalculator.py
+++ src/pythonCalculator.py
@@ -9,19 +9,17 @@ class Operation(Enum):
 def basicCalculator(operation, num1, num2):
     result = 0
 
-    match operation:
-        #if operation = ADD, return the sum
-        case Operation.ADD:
-            result = num1 + num2
-        case Operation.SUBTRACT:
-            result = num1 - num2
-        case Operation.MULTIPLY:
-            result = num1 * num2
-        case Operation.DIVIDE:
-            if num2 == 0:
-                print("Can't divide by 0")
-            else:
-                result = num1 / num2
+    if  operation == Operation.ADD:
+        result = num1 + num2
+    elif operation == Operation.SUBTRACT:
+        result = num1 - num2
+    elif operation == Operation.MULTIPLY:
+        result = num1 * num2
+    elif operation == Operation.DIVIDE:
+        if num2 == 0:
+            print("Can't divide by 0")
+        else:
+            result = num1 / num2
     return result
 
 
